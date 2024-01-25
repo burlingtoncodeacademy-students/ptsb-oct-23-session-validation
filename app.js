5b8aa36ea599a5d77e6e14a385fba2756d34d813
Paul Niemczyk <mrpndev@gmail.com>
2024-01-24 21:00:00 -0500

    Completed

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const HOST = "127.0.0.1"
 console.log(process.env)
 const authController = require("./controllers/auth")
 const routesController = require("./controllers/routes")
+const sessionValidation = require("./middlewares/session")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(authController)
-app.use(routesController)
+app.use(sessionValidation, routesController)
 
 app.listen(PORT, HOST, () => {
     dbConnect()
--- middlewares/session.js
+++ middlewares/session.js
@@ -4,13 +4,18 @@ const JWT_KEY = process.env.JWT_KEY
 
 const sessionValidation = async (req, res, next) => {
     try {
+        // Preflight request verifies if endpoint accepts HTTP methods
         if (req.method === "OPTIONS") next()
+        // Check if token has been provided
         if (!req.headers.authorization) throw new Error("Forbidden")
+        // Sanitized the token to remove the word 'Bearer' if it exists
         const authToken = req.headers.authorization.includes("Bearer")
             ? req.headers.authorization.split(" ")[1]
             : req.headers.authorization
+        // Extricate payload and verify token authenticity
         const payload = authToken ? jwt.verify(authToken, JWT_KEY) : undefined
         if (!payload) throw new Error("Invalid Token")
+        // Call users collection to find user matching payload id
         const foundUser = await Users.findOne({ _id: payload._id })
         if (!foundUser) throw new Error("User not found")
         req.user = foundUser
@@ -22,3 +27,5 @@ const sessionValidation = async (req, res, next) => {
         })
     }
 }
+
+module.exports = sessionValidation
